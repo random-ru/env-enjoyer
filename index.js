0cbb4394ef379bfe78557940e42fc1de725912ba
Elnar Ibragimov <4393143+Djelnar@users.noreply.github.com>
2021-09-17 03:43:44 +0300

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+"use strict";
+
+const path = require("path");
+const fs = require("fs");
+
+const filePath = path.resolve(process.cwd(), ".env");
+
+const SEPARATOR = "$$$###@@@";
+
+const envSource = fs.readFileSync(filePath, { encoding: "utf8" });
+
+const res = envSource.split("\n").join("").trim().split(SEPARATOR).join("\n");
+
+console.log(res);
+
+fs.writeFileSync(filePath, res, { encoding: "utf8" });
